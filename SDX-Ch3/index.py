7deb167ba2492eb20b06b4bc4947e2c24100ec80
Janet Davis <davisj@whitman.edu>
2024-01-30 10:20:38 -0800

    Create index.py

--- SDX-Ch3/index.py
+++ SDX-Ch3/index.py
@@ -1,16 +1,16 @@
-import sys, glob
+import sys, glob, os
 from hashlib import sha256
 
 def index(directory):
     """Generates an index of hashcodes for WAV files in a directory.
 
     Args:
-        directory (String): Name of the directory to search
+        directory (String): Name of the directory to index
         
     Returns: 
         {String}: Set of hashcodes for the WAV files in the top level of the directory 
     """
-    filenames = glob.glob(directory+'/*.wav')
+    filenames = glob.glob(os.path.join(directory,'*.wav'))
     
     hashcodes = set()
     for fn in filenames:
@@ -18,6 +18,17 @@ def index(directory):
         hashcodes.add(sha256(data).hexdigest())
     return hashcodes
 
+def writeIndex(directory):
+    """Writes hashcodes for WAV files in a directory to 
+    index.txt in that directory.
+
+    Args:
+        directory (String): Name of the directory to index
+    """
+    hashcodes = index(directory)
+    indexfilename = os.path.join(directory,'index.txt')
+    with open(indexfilename, 'w') as f:
+        f.write('\n'.join(hashcodes))
+
 if __name__ == "__main__":
-    directory = sys.argv[1]
-    print(index(directory))
+    writeIndex(sys.argv[1])
